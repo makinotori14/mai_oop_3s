0ceb31e2425f33b744d7d61cc11f20169272cbbb
Albert Han <makinotori14@gmail.com>
2025-11-07 20:21:58 +0300

    clear lab space

--- lab1/include/gcd.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdlib>
-#include <utility>
-
-namespace lab1 {
-    int gcd(int a, int b);
-}
--- lab1/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "gcd.h"
-
-int main() {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << lab1::gcd(a, b) << std::endl;
-    return 0;
-}
